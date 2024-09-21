b5deeda20d2175bf461ad1df402cba9b0cc7cebe
Monalisa-45 <monalisamohapatra9194@gmail.com>
2024-09-21 16:35:04 +0530

    updated home page

--- frontend/src/app/app.component.ts
+++ frontend/src/app/app.component.ts
@@ -2,7 +2,7 @@ import { Component } from '@angular/core';
 import { RouterLink, RouterOutlet } from '@angular/router';
 import { FooterComponent } from './footer/footer.component';
 import { NgIconComponent, provideIcons } from '@ng-icons/core';
-import { heroCheckBadge, heroCurrencyDollar, heroCurrencyRupee, heroDocumentCurrencyPound, heroDocumentCurrencyRupee, heroEnvelope, heroHome, heroPhone, heroShoppingBag, heroTruck, heroUsers } from '@ng-icons/heroicons/outline';
+import { heroCamera, heroCheckBadge, heroComputerDesktop, heroCurrencyDollar, heroCurrencyRupee, heroDevicePhoneMobile, heroDeviceTablet, heroDocumentCurrencyPound, heroDocumentCurrencyRupee, heroEnvelope, heroHome, heroMicrophone, heroPhone, heroShoppingBag, heroSwatch, heroTruck, heroUsers } from '@ng-icons/heroicons/outline';
 
 @Component({
   selector: 'app-root',
@@ -10,7 +10,7 @@ import { heroCheckBadge, heroCurrencyDollar, heroCurrencyRupee, heroDocumentCurr
   imports: [RouterOutlet, FooterComponent, RouterLink, NgIconComponent],
   templateUrl: './app.component.html',
   styleUrls: ['./app.component.css'],
-  viewProviders: [provideIcons({ heroUsers , heroPhone , heroEnvelope , heroCurrencyDollar , heroHome , heroShoppingBag , heroCurrencyRupee , heroTruck , heroCheckBadge})]    ///IDHAR LAGADEEE....JAISE YEH HAI ICON KA NAAM
+  viewProviders: [provideIcons({ heroUsers , heroPhone , heroEnvelope , heroCurrencyDollar , heroHome , heroShoppingBag , heroCurrencyRupee , heroTruck , heroCheckBadge , heroDevicePhoneMobile , heroComputerDesktop , heroSwatch , heroCamera , heroDeviceTablet})]    ///IDHAR LAGADEEE....JAISE YEH HAI ICON KA NAAM
 })
 
 export class AppComponent {
