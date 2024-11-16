8740379675f39b9073446dec730f8c52d8fd928a
arpita057 <arpitasahu2525@gmail.com>
2024-11-17 00:03:31 +0530

    Home page

--- frontend/tailwind.config.js
+++ frontend/tailwind.config.js
@@ -9,9 +9,15 @@ module.exports = {
           "0%, 100%": { opacity: "0" },
           "33%": { opacity: "1" },
         },
+        slide: {
+          "0%, 33%": { transform: "translateX(0)" },
+          "34%, 66%": { transform: "translateX(-1200px)" },
+          "67%, 100%": { transform: "translateX(-2400px)" },
+        },
       },
       animation: {
         fade: "fade 5s infinite",
+        slide: "slide 5s infinite", // Add the slide animation
       },
     },
   },
